fabc8df106c22e8c588f7cc08553980dca59f0b6
Ryan Spaeth <rspaeth@mvqn.net>
2019-08-08 11:24:32 -0700

    - Installed stylus and stylus-loader as dev dependencies. - Finally got sizing working as I wanted using stylus!

--- src/client/src/components/HtmlDesigner/HtmlDesigner.vue
+++ src/client/src/components/HtmlDesigner/HtmlDesigner.vue
@@ -10,6 +10,11 @@
             <slot></slot>
         </div>
 
+        <!--<div id="#blocks"></div>-->
+
+
+
+
     </div>
 
 </template>
@@ -58,26 +63,46 @@
                 //panels: { defaults: [] },
 
                 blockManager: {
-                    appendTo: '#blocks',
+                    //appendTo: '#blocks',
                     blocks: [
+                        /*
                         {
                             id: 'section', // id is mandatory
                             label: '<b>Section</b>', // You can use HTML/SVG inside labels
                             attributes: { class:'gjs-block-section' },
-                            content: `
-                                <section>
+                            content:
+                                `<section>
                                     <h1>This is a simple title</h1>
                                     <div>This is just a Lorem text: Lorem ipsum dolor sit amet</div>
-                                </section>
-                            `,
+                                </section>`,
                         },
+
+                         */
                         {
                             id: 'text',
                             label: 'Text',
-                            content: `
-                                <div data-gjs-type="text">Insert your text here</div>
-                            `,
+                            content: '<div data-gjs-type="text">Insert your text here</div>',
+                            category: "Test",
+                            attributes: {
+                                //class: "gjs-fonts gjs-f-b1",
+                                //class: "gjs-fonts gjs-f-b2",
+                                //class: "gjs-fonts gjs-f-b3",
+                                //class: "gjs-fonts gjs-f-b37",
+                                class: "gjs-fonts gjs-f-text",
+                                //class: "fa fa-link",
+                                //class: "gjs-fonts gjs-f-image",
+                                //class: "fa fa-youtube-play",
+                                //class: "fa fa-map-o",
+                                //class: "fa fa-youtube-play",
+                                //class: "fa fa-quote-right",
+                                //class: "gjs-fonts gjs-f-h1p",
+
+                                // For others, inspect code on: https://grapesjs.com/demo.html
+                                // Also check out: https://github.com/artf/grapesjs-blocks-basic
+                            }
+
                         },
+                        /*
                         {
                             id: 'image',
                             label: 'Image',
@@ -92,6 +117,8 @@
                             // reacts by opening the AssetManager
                             activate: true,
                         }
+                        */
+
                     ]
                 },
 
@@ -113,21 +140,15 @@
     rem2px(value)
         unit(value) is "rem" ? unit(value * 16, "px") : unit(value, unit(value))
 
-    editor-min-width = 576px - rem2px(2rem) + 2px // 546px
-
-    canvas-min-width = 320px
-    panels-min-width = editor-min-width - canvas-min-width // 226px
-
-    canvas-min-percentage = unit( ( 320px / editor-min-width ) * 100, "%")
-    panels-min-percentage = 100% - canvas-min-percentage
-
-
-    .gjs-devices-c
-        padding 0
+    editor-min-width        = 576px - rem2px(2rem) + 2px                            // 546px
+    canvas-min-width        = 320px                                                 // 320px
+    panels-min-width        = editor-min-width - canvas-min-width                   // 226px
+    canvas-min-percentage   = unit( ( 320px / editor-min-width ) * 100, "%")        // 58.60805860805861%
+    panels-min-percentage   = 100% - canvas-min-percentage                          // 41.39194139194139%
 
     @media screen and (min-width: 576px)
         .gjs-pn-views, .gjs-pn-views-container
-            min-width panels-min-width
+            //min-width panels-min-width
             width panels-min-percentage
         .gjs-pn-options
             right panels-min-percentage
@@ -147,36 +168,26 @@
     //@media screen and (min-width: 1200px)
 
 
-
+    .gjs-devices-c
+        width 170px
+        padding 0
 
 
     .gjs-pn-buttons
-    {
-        justify-content: flex-end;
-    }
+        justify-content flex-end
+        > span:last-child
+            margin-right 0
 
-    .gjs-pn-buttons > span:last-child
-        margin-right 0
-
-
-
-    /* Override the template's min-width! */
-    #content-container {
-        //min-width: 495px !important;
-
-    }
 
     .gjs-am-file-uploader form
-    {
-        height: 325px; /* Match the right panel? */
-        margin-bottom: 0;
-    }
+        height 325px
+        margin-bottom 0
 
-    #gjs-am-uploadFile {
-        position: relative;
-        height: 100%;
-        border: none; /* The form handles this above! */
-    }
+    #gjs-am-uploadFile
+        position relative
+        height 100%
+        border none
+
 
     /* TODO: Fix buttons in Style Manager! */
 
